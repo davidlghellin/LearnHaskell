3d501da3857d08ca452fc3e42e1f4efd405e63ca
David López <davidlghellin@gmail.com>
2021-01-22 19:15:50 +0100

    HaskellBook init

---
+++ HaskellBook/src/Cap02/MyFunctions.hs
@@ -0,0 +1,4 @@
+module Cap02.MyFunctions where
+
+triple :: Integer -> Integer
+triple x = x * 3
---
+++ HaskellBook/test/Tests.hs
@@ -0,0 +1,12 @@
+module Main where
+
+
+import Test.Tasty
+import Test.Tasty.HUnit
+import Cap02.MyFunctionsTest
+
+misTest :: TestTree
+misTest = testGroup "Mis test totales" [ misTestFuns
+                                       ]
+
+main = defaultMain misTest
