a8ec5986c2de53626062d924a81fb33766cc43f1
David López <davidlghellin@gmail.com>
2023-10-30 21:36:43 +0100

    Configuración básica del curso de youtube y el devcontainer

---
+++ CursoHaskellDesdeCeroYoutube/README.md
@@ -0,0 +1,8 @@
+# Aprendiendo Haskell
+
+https://www.youtube.com/playlist?list=PLD2wfKpqmxnkHPK_Tzv80HCwOaYph33pH
+
+```bash
+cd curso_cap0
+ghci ejemplo_00.hs
+```
---
+++ CursoHaskellDesdeCeroYoutube/curso_cap0/ejemplo_00.hs
@@ -0,0 +1,9 @@
+module Ejemplo_00 where
+
+-- nombre :: argumentosX -> tipoRetorno 
+-- nombre :: argumentos1 -> argumentos2 -> tipoRetorno 
+division :: Int -> Int -> Int
+division x y = x `div` y 
+
+cuadrado :: Int -> Int
+cuadrado x = x * x
