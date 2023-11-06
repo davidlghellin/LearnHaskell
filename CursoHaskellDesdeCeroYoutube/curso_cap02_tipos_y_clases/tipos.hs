bc2af320931ca40ba009ce77d596c008bbd003e1
David López <davidlghellin@gmail.com>
2023-11-06 20:47:25 +0000

    Tipos

---
+++ CursoHaskellDesdeCeroYoutube/curso_cap02_tipos_y_clases/tipos.hs
@@ -0,0 +1,13 @@
+module Tipos where
+
+-- v :: T
+-- True :: Bool
+-- podemos decir, v tiene el tipo T
+-- El operador :: puede dar el tipo de expresiones no evaluadas
+
+{-
+Antes de evaluar comprueba los tipos
+  - Inferencia de tipos
+Gracias a eso elimina los errores en tiempo de ejecución en los tipos
+  - 
+-}
